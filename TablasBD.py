88b272d22261afac8d1e5bb562277aed561f4363
benjamackay <167359753+benjamackay@users.noreply.github.com>
2024-06-22 18:19:31 -0400

    Update TablasBD.py

--- TablasBD.py
+++ TablasBD.py
@@ -1,62 +1,75 @@
 import psycopg2
-from psycopg2 import sql
+
 
 conn = psycopg2.connect(
-    host = 'localhost',
-    user= 'postgres',
-    dbname = 'tarea2',
-    password = '', #contraseña
-    port = '5432'
+    host='localhost',
+    user='benjajjj',
+    dbname='tarea2',
+    password='' #contraseña
 )
 print("Conexion Exitosa")
+
 cursor = conn.cursor()
 
-create_tables_queries = [
-    """
-    CREATE TABLE IF NOT EXISTS usuarios (
+
+create_tables_queries = ["""
+   CREATE TABLE IF NOT EXISTS usuarios (
     id_usuario SERIAL PRIMARY KEY NOT NULL,
     descripcion VARCHAR(255),
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     nombre VARCHAR(35) NOT NULL,
     direccion_correo VARCHAR(255) UNIQUE NOT NULL,
-    contraseña TEXT NOT NULL,
-    es_admin BOOLEAN NOT NULL DEFAULT FALSE 
+    contraseña TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS correos (
+CREATE TABLE IF NOT EXISTS correos(
     id_correo SERIAL PRIMARY KEY,
-    remitente INT NOT NULL,
-    destinatario INT NOT NULL,
+    remitente VARCHAR(255) NOT NULL,
+    destinatario VARCHAR(255) NOT NULL,
     asunto VARCHAR(255),
     cuerpo TEXT NOT NULL,
     fecha_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     leido BOOLEAN DEFAULT FALSE,
     es_favorito BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (remitente) REFERENCES usuarios(id_usuario),
-    FOREIGN KEY (destinatario) REFERENCES usuarios(id_usuario)
+    FOREIGN KEY (remitente) REFERENCES usuarios(direccion_correo),
+    FOREIGN KEY (destinatario) REFERENCES usuarios(direccion_correo)
 );
 
 CREATE TABLE IF NOT EXISTS correos_favoritos (
-    id_correo_favorito SERIAL PRIMARY KEY,
-    usuario_id INT NOT NULL,
-    correo_id INT NOT NULL,
+    id_correo_favorito INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    direccion_correo VARCHAR(255) NOT NULL,
+    id_corre  INT NOT NULL,
+    clave TEXT NOT NULL,
     fecha_anadido TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id_usuario),
-    FOREIGN KEY (correo_id) REFERENCES correos(id_correo)
+    FOREIGN KEY (correo) REFERENCES usuarios(direccion_correo),
+    FOREIGN KEY (id_corre) REFERENCES correos(id_correo)
 );
 
 CREATE TABLE IF NOT EXISTS usuarios_bloqueados (
     id_bloqueado SERIAL PRIMARY KEY,
-    usuario_bloqueado INT NOT NULL,
-    usuario_id INT NOT NULL,
+    correo VARCHAR(255) NOT NULL,
+    correo_bloqueado VARCHAR(255) NOT NULL,
     fecha_bloqueo TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (usuario_bloqueado) REFERENCES usuarios(id_usuario),
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id_usuario)
+    clave TEXT NOT NULL,
+    FOREIGN KEY (correo) REFERENCES usuarios(direccion_correo),
+    FOREIGN KEY (correo_bloqueado) REFERENCES usuarios(direccion_correo)
 );
-    """
+"""
 ]
 
-cursor.execute(create_tables_queries)
+
+for query in create_tables_queries:
+    cursor.execute(query)
+
+
+conn.commit()
+
+
+cursor.close()
+conn.close()
+
+print("Tablas creadas exitosamente")
+
 conn.commit()
 
 cursor.close()
